7f4139a550085a7b8d9c14a388637c479a8711d6
DaoXuanHuong <DaoXuanHuong@DESKTOP-Q8K3OFI>
2021-01-22 15:23:29 +0700

    Create Database

---
+++ HpShopSolution.Data/EF/HpShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HpShopSolution.Data.EF
+{
+    public class HpShopDbContextFactory : IDesignTimeDbContextFactory<HpShopDbContext>
+    {
+        public HpShopDbContext CreateDbContext(string[] args)
+        {
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            var connectionString = configuration.GetConnectionString("HpShopSolutionDb");
+            var optionsBuilder = new DbContextOptionsBuilder<HpShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new HpShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
